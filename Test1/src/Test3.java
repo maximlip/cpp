89100d8c7924b0080ab97280485e342a8958de99
maxlip <User@MaxDell>
2021-02-05 15:30:32 +0200

    add files

---
+++ Test1/src/Test3.java
@@ -0,0 +1,47 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Stream;
+
+public class Test3 {
+	static String n = "5";
+	
+	static void method() {
+		System.out.println(Thread.currentThread().getName());
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+
+		System.out.println(n);
+		Thread.currentThread();
+
+		Runnable task = () -> {
+			System.out.println(Thread.currentThread().getName());
+		};
+
+		new Thread(task).start();
+
+
+		new Thread(() -> System.out.println(Thread.currentThread().getName())).start();
+		
+		Set<String> ts = new TreeSet<String>((String a, String b) -> {return a.compareTo(b);});
+		ts.add("Aaaaaaa");
+		ts.add("bbb");
+		System.out.println(ts);
+		
+		//System.in.read()
+		
+		Stream<String> st = ts.stream().peek(s -> System.out.println(s));
+		System.out.println(st);
+		System.out.println(Arrays.toString(st.toArray()));
+		
+		new Thread(Test3::method) {}.start();
+		
+		Thread.sleep(10000);
+		
+		Stream<String> st2 = ts.stream().filter(s -> s.length() > 2);
+		System.out.println(Arrays.toString(st2.toArray()));
+	}
+
+}
